f75acbb7c95cc7ec1b6d20bd84baf9411039fd9e
paerma <555rostik556@ukr.net>
2026-05-18 18:33:50 +0200

    fix: no need of "this" statement

--- src/main/java/com/example/blackjack/Dealer.java
+++ src/main/java/com/example/blackjack/Dealer.java
@@ -5,7 +5,7 @@ public class Dealer extends Hand {
     @Override
     public void takeCards(Deck deck) {
         if(getPoints()<17){
-            deck.giveCard(this);
+            deck.giveCard();
         }
     }
 }
