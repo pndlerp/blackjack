ac81ada96bc04343b773733867bb4833c80f036c
paerma <555rostik556@ukr.net>
2026-05-21 22:50:56 +0200

    updated ui

--- src/main/java/com/example/blackjack/BlackJackFX.java
+++ src/main/java/com/example/blackjack/BlackJackFX.java
@@ -11,7 +11,8 @@ public class BlackJackFX extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BlackJackFX.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+        stage.setResizable(false);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
