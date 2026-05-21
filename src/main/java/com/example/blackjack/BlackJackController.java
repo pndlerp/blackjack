3650cab72a3d2ad14de0927f39f37f2b28b105b5
paerma <555rostik556@ukr.net>
2026-05-21 22:50:05 +0200

    added ui animations, endgame screen

--- src/main/java/com/example/blackjack/BlackJackController.java
+++ src/main/java/com/example/blackjack/BlackJackController.java
@@ -1,37 +1,49 @@
 package com.example.blackjack;
 
+import javafx.animation.PauseTransition;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.List;
 
 public class BlackJackController {
-    @FXML
-    private VBox menu, mainStage;
-    @FXML
-    private Text DealerCard1, DealerCard2, DealerCard3, PlayerCard1, PlayerCard2, PlayerCard3;
-    @FXML
-    private HBox dealerCardsBox, playerCardsBox;
-    @FXML
-    private Button stand;
+    @FXML private VBox menu, mainStage;
+    @FXML private HBox dealerCardsBox, playerCardsBox;
+    @FXML private Button stand, hit;
+    @FXML private Text playerScore, dealerScore;
     private Table table;
 
 
     @FXML
     protected void onStartButtonClick() {
+        PauseTransition pause = new PauseTransition(Duration.seconds(0.5));
+
 
         playerCardsBox.setSpacing(15);
         dealerCardsBox.setSpacing(15);
         menu.setVisible(false);
         table = new Table();
         table.startNewGame();
-        UpdateUI();
+        pause.setOnFinished(event -> {
+                    table.hitDealer();
+                    UpdateUI();
+                    onHitClick();
+                });
+        pause.play();
+
+        onHitClick();
+
+        stand.setDisable(false);
+        hit.setDisable(false);
 
         mainStage.setVisible(true);
 
@@ -39,25 +51,61 @@ public class BlackJackController {
 
     @FXML
     protected void onHitClick() {
-        table.hitPlayer();
-        UpdateUI();
-        if(!table.ifCanContinue()){
-            onStandClick();
-        }
+        PauseTransition pause = new PauseTransition(Duration.seconds(0.3));
+        hit.setDisable(true);
+        pause.setOnFinished(event -> {
+            table.hitPlayer();
+            UpdateUI();
+            hit.setDisable(false);
+            if (!table.ifCanContinue()) {
+                onStandClick();
+            }
+        });
+        pause.play();
 
     }
 
     @FXML
     protected void onStandClick() {
+        PauseTransition pause = new PauseTransition(Duration.seconds(0.75));
         stand.setDisable(true);
+        hit.setDisable(true);
+        pause.setOnFinished(event -> {
         table.getDealer().addHiddenCardToList();
         UpdateUI();
-        while(table.getDealer().getPoints() < 17) {
-            table.hitDealer();
-            UpdateUI();
-        }
-        System.out.println(table.checkWinCondition());
+        });
+        pause.play();
+        pause.setOnFinished(event -> {
+        UpdateUI();
 
+                    if (table.getDealer().getPoints() < 17) {
+                        table.hitDealer();
+                        UpdateUI();
+                        pause.play();
+                    }
+                    else {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Game result");
+                        switch (table.checkWinCondition()) {
+                            case DEALER_WIN -> alert.setHeaderText("You lost.");
+                            case PLAYER_WIN -> alert.setHeaderText("You won!");
+                            case DRAW -> alert.setHeaderText("Draw.");
+                        }
+                        alert.setContentText("Press OK to start a new game");
+                        Platform.runLater(() -> {
+                            alert.showAndWait();
+                            onStartButtonClick();
+                        });
+                    }
+                });
+        pause.play();
+
+
+
+    }
+
+    @FXML
+    protected void endScreen(){
 
     }
 
@@ -67,11 +115,15 @@ public class BlackJackController {
 
         List<Card> cards = table.getDealer().getCards();
 
+        dealerScore.setText("Dealer Score: " + String.valueOf(table.getDealer().getPoints()));
+
         CardsUI(cards, dealerCardsBox);
 
         playerCardsBox.getChildren().clear();
 
         List<Card> cards1 = table.getPlayer().getCards();
+
+        playerScore.setText("Player Score: " +String.valueOf(table.getPlayer().getPoints()));
 
         CardsUI(cards1, playerCardsBox);
     }
